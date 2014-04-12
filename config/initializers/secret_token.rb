4af2592faa0ba1d1490963127b384b98e7e1d615
Daniel Greenberg <dgreenbe777@gmail.com>
2014-04-12 10:51:23 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Applify::Application.config.secret_key_base = 'da0dfe73c0511cfceb4e5a98e38ca41688b3719796ba739ca94f97fec0ed7a183f6e406f543aad3a990fa9e7799274cfd6c8f1902034cb80608e12140f8d5093'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Applify::Application.config.session_store :cookie_store, key: '_applify_session'
